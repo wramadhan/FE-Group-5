f0da74392d6ebf464654dc472d37c0ba0e2303e3
zainal547 <zainal547@gmail.com>
2022-07-26 05:34:54 +0700

    Menambahkan logic pada file register

--- src/pages/Registrasi.jsx
+++ src/pages/Registrasi.jsx
@@ -1,17 +1,45 @@
-import {useState, useEffect} from 'react' ;
-import appLogo from '../images/mychat.png';
-import '../styles/Registrasi.css'
+import React, { useState } from "react";
+import appLogo from "../images/mychat.png";
+import "../styles/Registrasi.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Registrasi =()=>{
-    // const [userName, setUserName] = useState('');
+const Registrasi = () => {
+  //1
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [file, setFile] = useState("");
+  //2
+  const navigate = useNavigate();
 
-    return(
-        <div className="container w-full h-screen">
-            <div className="flex">
-        <img className='appLogo' src={appLogo} alt='appLogo'/>
-        <h1 className='daftar' >Daftar Sekarang</h1>
-            </div>
-        </div>
-    )
-}
+  //3
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`https://app.swaggerhub.com/apis/GolangProject/SosialMedia/1.0.1/users`, {
+        username: "asd",
+        password: "asd",
+        email: "email@mail.com",
+        fullname: "asd",
+        file: "asd",
+      })
+      .then((response) => {
+        alert("Good Job!", "Success Sign Up", "Success");
+      })
+      .catch(function (err) {
+        alert(err);
+      });
+  };
+
+  return (
+    <div className="container w-full h-screen">
+      <div className="flex">
+        <img className="appLogo" src={appLogo} alt="appLogo" />
+        <h1 className="daftar">Daftar Sekarang</h1>
+      </div>
+    </div>
+  );
+};
 export default Registrasi;
