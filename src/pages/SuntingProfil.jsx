6515f5603f15d0009d769bf867ca10b316c69c99
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-26 01:29:47 +0700

    add image to folder image

---
+++ src/pages/SuntingProfil.jsx
@@ -0,0 +1,9 @@
+// import {useState, useEffect} from "react";
+import '../styles/SuntingProfil.css'
+
+const SuntingProfil = () => {
+    return(
+        <div></div>
+    )
+}
+export default SuntingProfil;
