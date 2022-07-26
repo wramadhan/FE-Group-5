d3b814bd56b0bcefe3b96f6091576fd7a96c79c1
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-27 00:20:31 +0700

    navbar styling was cleared

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -8,12 +8,15 @@ const NavBar =()=>{
     return(
         <div className="navbar">
             <img className='keluar' src={keluar} alt="Tombol Keluar" />
+            <div className="tengah">
             <img className='appLogoPutih' src={appLogoPutih} alt="appLogoPutih" />
             <h1 className='subjek'>Halaman Utama</h1>
-            <>
-            <p>Hai Pengguna,<br />Hari yang indah</p>
+            </div>
+            <div className="kanan">
+            <p className='user'>Hai Pengguna,<br />Hari yang indah</p>
             <img className='profil' src={profil} alt="profil" />
-            </>
+            </div>
+            
         </div>
     )
 }
