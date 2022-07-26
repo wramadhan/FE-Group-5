4f307071b151fc7a2993d2ef46fd354ddc86346f
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-26 13:53:32 +0700

    revisi login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
-// import SuntingProfil from "./pages/SuntingProfil";
-import Registrasi from "./pages/Registrasi";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 // import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Registrasi />
+  <Login />
   // </React.StrictMode>
 );
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import Header from '../component/Header';
+import '../styles/Login.css'
 const Login = () => {
   return (
+    <>
+    <Header />
     <div className="items-center text-center justify-between ">
       <label className="">
         <input
@@ -18,28 +21,25 @@ const Login = () => {
       <label className="">
         <input
           type="password"
-          id="inputID"
+          id="inputPWD"
           name="password"
           className="mt-1 px-3 py-2 bg-white border mx-auto  border-slate-300 placeholder-slate-400 
           focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-1/2 rounded-md sm:text-sm focus:ring-1"
           placeholder="Kata Sandi"
-        />
+          />
         <br />
       </label>
 
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Button
       </button>
       <p>
-        belum memiliki akun? <br />{" "}
-        <div className="text-blue ">
-          <span className="line">
-            {/* put router link here */}
-            <a href="www.facebook.com">Daftar Sekarang</a>{" "}
-          </span>{" "}
-        </div>
+        belum memiliki akun?
       </p>
-    </div>
+      <p className="daftar ">Daftar Sekarang
+      </p>
+</div>
+          </>
   );
 };
 export default Login;
