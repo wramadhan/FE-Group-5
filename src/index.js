6e702b332861615ed711596cddd2ae7bf37e18e5
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-26 21:05:20 +0700

    uninstall tailwind

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import NavBar from "./component/NavBar";
 // import "./index.css";
-// import SuntingProfil from "./pages/SuntingProfil";
-import Registrasi from "./pages/Registrasi";
-// import Login from "./pages/Login";
+// import Registrasi from "./pages/Registrasi";
 // import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Registrasi />
+  <NavBar />
   // </React.StrictMode>
 );
 
