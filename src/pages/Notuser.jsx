9cedffeb6fabce465e9c3e24818dd8eb1db7989e
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-27 22:40:38 +0700

    not user was cleared

--- src/pages/Notuser.jsx
+++ src/pages/Notuser.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+// import { Button, Card } from "react-bootstrap";
 import appLogo from "../images/suntingprofil.png";
 import "../styles/NotUser.css";
 import NavBar from "../component/NavBar";
@@ -10,41 +10,49 @@ const Notuser = () => {
       <NavBar />
       <div className="Border2">
         <div className="HeadComment">
-          <Card>
+          {/* <Card> */}
+          <div>
             <img className="img1" src={appLogo} alt="appLogo" />
-            <Card.Header className="cardhead">Nama Penguna Lain</Card.Header>
-          </Card>
+            {/* <Card.Header className="cardhead">Nama Penguna Lain</Card.Header> */}
+            <div className="cardhead">Nama Penguna Lain</div>
+          </div>
+          {/* </Card> */}
         </div>
         <br />
         <div className="status">
-          <Card className="lorem">
+          <div className="lorem">
+          {/* <Card> */}
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
             quos fugiat velit enim alias mollitia tempora ullam repellat optio
             harum tempore beatae ipsam, eos corrupti omnis suscipit similique
             eligendi doloremque natus nobis exercitationem! Numquam officia quod
             placeat veritatis molestias?
-          </Card>
+          {/* </Card> */}
+          </div>
         </div>
-        <Button className="ButtonCmt"> komentar</Button>{" "}
+        {/* <Button className="ButtonCmt"> komentar</Button>{" "} */}
+        <button className="ButtonCmt"> komentar</button>
         <form>
           <textarea placeholder="Ketik komentar pada kolom ini"></textarea>
         </form>
-        <Button className="ButtonPst">Posting</Button>{" "}
+        <button className="ButtonPst">Posting</button>
+        {/* <Button className="ButtonPst">Posting</Button>{" "} */}
         <div className="status2">
           <div className="HeadComment">
-            <Card>
+            <div>
               <img className="img2" src={appLogo} alt="appLogo" />
-              <Card.Header className="cardhead2">Nama komentator</Card.Header>
-            </Card>
+              {/* <Card.Header className="cardhead2">Nama komentator</Card.Header> */}
+              <div className="cardhead2">Nama komentator</div>
+            </div>
           </div>
           <br />
-          <Card className="lorem">
+          <div className="lorem">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
             quos fugiat velit enim alias mollitia tempora ullam repellat optio
             harum tempore beatae ipsam, eos corrupti omnis suscipit similique
             eligendi doloremque natus nobis exercitationem! Numquam officia quod
             placeat veritatis molestias?
-          </Card>
+          </div>
         </div>{" "}
       </div>
     </>
