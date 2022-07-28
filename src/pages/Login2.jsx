11ea5c0239413b8593322fdde939421a0c0ec574
zainal547 <zainal547@gmail.com>
2022-07-28 20:36:59 +0700

    Menambahkan file logic di login

--- src/pages/Login2.jsx
+++ src/pages/Login2.jsx
@@ -1,24 +1,52 @@
-import '../styles/Login2.css';
-import mychat from '../images/mychat.png';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/Login2.css";
+import mychat from "../images/mychat.png";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login2 =()=> {
-return(
+const Login2 = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("username", "asd");
+    formData.append("password", "asd");
+    axios
+      .post("https://virtserver.swaggerhub.com/GolangProject/SosialMedia/1.0.1/users/", formData, { headers: { "Content-Type": "multipart/form-data" } })
+      .then((response) => {
+        console.log(response);
+        alert("Good job!", "Succecss Sign Up", "success");
+        navigate("/", { replace: true });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+    //.finally(() => {});
+  };
+  return (
     <div className="bg1">
-        <div className="wrapper1">
-    <img src={mychat} alt="mychat" className='mychat1'/>
-    <h2 className='subjek1'>My Chat</h2>
-        </div>
-    <form className='form1'action="#">
-        <input type="text" placeholder='E-mail' className='input1'/>
-        <input type="text" placeholder='Kata Sandi' className='input1'/>
-        <button class='submit1'type="submit" value="Submit"><Link to='/home'>Masuk</Link></button>
-    </form>
-    <div className="bawah1">
-    <p className='belum1'>Belum mendaftar?</p>
-    <p className='daftar1'><Link to='/registrasi'>Daftar sekarang</Link></p>
+      <div className="wrapper1">
+        <img src={mychat} alt="mychat" className="mychat1" />
+        <h2 className="subjek1">My Chat</h2>
+      </div>
+      <form className="form1" action="#" onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="E-mail" className="input1" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" placeholder="Kata Sandi" className="input1" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button className="submit1" type="submit" value="Submit">
+          <Link to="/">Masuk</Link>
+        </button>
+      </form>
+      <div className="bawah1">
+        <p className="belum1">Belum mendaftar?</p>
+        <p className="daftar1">
+          <Link to="/registrasi">Daftar sekarang</Link>
+        </p>
+      </div>
     </div>
-    </div>
-)
+  );
 };
 export default Login2;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Registrasi from "../pages/Registrasi"
+import Registrasi from "../pages/Registrasi";
 import SuntingProfil from "../pages/SuntingProfil";
-import KomentarPenggguna from "../pages/KomentarPengguna"
+import KomentarPenggguna from "../pages/KomentarPengguna";
 import Login2 from "../pages/Login2";
-import KomentarPengggunaLain from "../pages/KomentarPenggunaLain"
+
+import KomentarPengggunaLain from "../pages/KomentarPenggunaLain";
 import HalamanUtama from "../pages/HalamanUtama";
 
-const App =() => {
-return(
-<BrowserRouter>
-<Routes>
-    <Route path="/" element={<Login2 />} />
-    <Route path="/home" element={<HalamanUtama />} />
-    <Route path="/registrasi" element={<Registrasi />} />
-    <Route path="/sunting" element={<SuntingProfil />} />
-    <Route path="/komentar/id" element={<KomentarPenggguna />} />
-    <Route path="/komentar/ln" element={<KomentarPengggunaLain />} />
-</Routes>
-</BrowserRouter>
-)
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HalamanUtama />} />
+        <Route path="/login" element={<Login2 />} />
+        <Route path="/registrasi" element={<Registrasi />} />
+        <Route path="/sunting" element={<SuntingProfil />} />
+        <Route path="/komentar/id" element={<KomentarPenggguna />} />
+        <Route path="/komentar/ln" element={<KomentarPengggunaLain />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default App;
